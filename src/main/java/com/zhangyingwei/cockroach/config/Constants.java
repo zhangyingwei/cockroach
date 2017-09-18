11e82e0a688469e010550690ae8d47b8980bc4b6
zhangyw <zhangyw001@gmail.com>
2017-09-18 11:52:16 +0800

    修改了一个错别字

--- src/main/java/com/zhangyingwei/cockroach/config/Constants.java
+++ src/main/java/com/zhangyingwei/cockroach/config/Constants.java
@@ -2,7 +2,7 @@ package com.zhangyingwei.cockroach.config;
 
 /**
  * Created by zhangyw on 2017/9/13.
- * 常亮统一管理
+ * 常量统一管理
  */
 public class Constants {
     public static final String APP_NAME_KEY = "cockroach.app.name";
