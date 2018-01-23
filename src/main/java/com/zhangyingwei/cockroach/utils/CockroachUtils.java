169b3b68f067bdaf08e9dc62c0535c744e90dcd4
zhangyingwei <zhangyw_001@126.com>
2018-01-23 19:13:44 +0800

    去掉无关依赖

--- src/main/java/com/zhangyingwei/cockroach/utils/CockroachUtils.java
+++ src/main/java/com/zhangyingwei/cockroach/utils/CockroachUtils.java
@@ -1,7 +1,5 @@
 package com.zhangyingwei.cockroach.utils;
 
-import com.zhangyingwei.cockroach.watcher.CgLibProxy;
-import net.sf.cglib.proxy.Enhancer;
 
 /**
  * Created by zhangyw on 2017/9/14.
@@ -14,21 +12,5 @@ public class CockroachUtils {
 
     public static String exceptionMessage(int code,String message){
         return String.format("code:%d - message:%s",code,message);
-    }
-
-    public static <T> T createBean(Class clazz){
-        CgLibProxy proxy = new CgLibProxy();
-        Enhancer enhancer = new Enhancer();
-        enhancer.setSuperclass(clazz);
-        enhancer.setCallback(proxy);
-        return (T) enhancer.create();
-    }
-
-    public static <T> T createBean(Class clazz,Class[] argTypes, Object[] args) {
-        CgLibProxy proxy = new CgLibProxy();
-        Enhancer enhancer = new Enhancer();
-        enhancer.setSuperclass(clazz);
-        enhancer.setCallback(proxy);
-        return (T) enhancer.create(argTypes,args);
     }
 }
